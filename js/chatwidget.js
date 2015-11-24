b420e188100196c16f5935bbbc88144663029a61
Data1us <hockey.aaron@gmail.com>
2015-11-25 00:04:14 +1000

    adding some tab stuff

---
+++ js/chatwidget.js
@@ -0,0 +1,26 @@
+(function(window) {
+	
+	if (typeof(window.page) == "undefined") window.page = {};
+	if (typeof(window.page.widgets) == "undefined") window.page.widgets = {};
+	
+	
+	page.chatwidget = (function(options){
+		var target = $(options.target);		
+		var source   = '<iframe src="{{url}}" class="chat-iframe"></iframe>';
+		var template = Handlebars.compile(source);
+		var el = {
+			target:target
+		}
+		
+		var render = function(){
+			if (target.find("iframe").length !== 0) return;
+			target.html(template({url:options.url}));		
+		}
+		
+		return {
+			el: el,
+			render: render
+		}
+	})
+	
+})(window, Handlebars);
