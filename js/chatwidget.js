2bd47c4c2d272314b7714ed848f76434a566066c
Data1us <hockey.aaron@gmail.com>
2016-04-19 23:02:53 +1000

    yup
    Added solution and did a whole bunch of stuff.  Created destroy option
    the chat widget,  and also a persist.

--- js/chatwidget.js
+++ js/chatwidget.js
@@ -2,26 +2,44 @@
 	
 	if (typeof(window.page) == "undefined") window.page = {};
 	if (typeof(window.page.widgets) == "undefined") window.page.widgets = {};
-	
-	
-	page.chatwidget = (function(options){
-		var target = $(options.target);		
-		var source   = '<iframe src="{{url}}" class="chat-iframe"></iframe>';
-		var template = Handlebars.compile(source);
-		var el = {
-			target:target
-		}
-		
-		var render = function(){
-			var forceRender = typeof(options.forceRender) != "undefined" ? options.forceRender : false;
-			if (!forceRender && target.find("iframe").length !== 0) return;
-			target.html(template({url:options.url}));		
-		}
-		
-		return {
-			el: el,
-			render: render
-		}
-	})
-	
+
+
+    page.chatwidget = (function(options) {
+        var target = $(options.target);
+        var source = (typeof (options.customTemplate) !== "undefined")
+            ? options.customTemplate
+            : '<img class="loaderimg" src="/img/hex-loader2.gif"/><iframe src="{{url}}" class="chat-iframe hidden"></iframe>';
+        var template = Handlebars.compile(source);
+        var el = {
+            target: target,
+            options: options
+        }
+
+        var render = function() {
+            var self = this;
+            var forceRender = typeof(options.forceRender) != "undefined" ? options.forceRender : false;
+            if (!forceRender && target.find("iframe").length !== 0) return;
+            target.html(template({ url: options.url }));
+            var loadTime = typeof (self.el().options.loadTime) !== "undefined"
+                ? self.el().options.loadTime
+                : 1000;
+            setTimeout(function() {
+                self.el().target.find(".loaderimg").addClass("hidden");
+                self.el().target.find("iframe").removeClass("hidden");
+            },loadTime);
+            
+        }
+
+        var destroyMe = function() {
+            var self = this;
+            self.el().target.html("");
+        };
+
+        return {
+            el: function() { return el; },
+            render: render,
+            destroyMe: destroyMe
+        }
+    });
+
 })(window, Handlebars);
